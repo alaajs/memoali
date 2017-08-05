074d380db5e7d34137d7975f3c9e9333879f97c7
alaajs <30490322+alaajs@users.noreply.github.com>
2017-08-05 11:40:09 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,34 @@
+do local _ = {
+  admins = {
+    {
+      0
+    },
+    {
+      189161839,
+      "@iq\\_313k"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "reply",
+    "echo",
+    "addreplay",
+    "update",
+    "me"
+  },
+  info_text = "",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    266714448,
+    0
+  }
+}
+return _
+end
